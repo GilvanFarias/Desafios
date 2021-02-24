ef0195b7bc70bb1c708b38bbc89803b292346b86
Gilvan Farias <63085957+GilvanFarias@users.noreply.github.com>
2021-02-23 23:50:02 -0300

    desafio 01 c# resolvido

---
+++ C#/Desafio 01.cs
@@ -0,0 +1,14 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        /*Seu primeiro programa em qualquer linguagem de programação 
+        é geralmente " Hello World! ". 
+        Neste primeiro problema basta imprimir esta mensagem na tela.*/
+
+        Console.WriteLine("Hello Word!");
+    }
+
+}
